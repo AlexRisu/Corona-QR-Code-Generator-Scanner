4a19557c40ea92058a7e3b8ae79696ddbd038b91
AlexRisu <abirkhb@gmail.com>
2021-01-28 12:11:04 +0100

    main-activity: activity_main.xml dummy design hinzufügen

--- app/src/main/java/com/example/coronaqrcodegenerator/MainActivity.kt
+++ app/src/main/java/com/example/coronaqrcodegenerator/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.coronaqrcodegenerator
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun routeToCodeCreator(view: View) {
+        println("Creator")
+    }
+
+    fun routeToCodeScanner(view: View) {
+        println("Scanner")
     }
 }
