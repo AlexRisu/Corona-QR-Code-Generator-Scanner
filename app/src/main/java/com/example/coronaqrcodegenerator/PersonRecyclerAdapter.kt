bdfed10baa60024cf22353bab7c0e459cb6a9e0e
AlexRisu <abirkhb@gmail.com>
2021-03-11 13:27:59 +0100

    person-recycler-adapter-adjustments: changing PersonRecyclerAdapters holder from List<Person> to PersonDbHelper + adding updateRecyclerView method to update and notify the data

--- app/src/main/java/com/example/coronaqrcodegenerator/PersonRecyclerAdapter.kt
+++ app/src/main/java/com/example/coronaqrcodegenerator/PersonRecyclerAdapter.kt
@@ -6,11 +6,14 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.coronaqrcodegenerator.models.Person
+import com.example.coronaqrcodegenerator.models.persistence.PersonDbHelper
 
-class PersonRecyclerAdapter(private var items: List<Person>) :
+class PersonRecyclerAdapter(private var personDbHelper: PersonDbHelper) :
     RecyclerView.Adapter<PersonRecyclerAdapter.PersonViewHolder>() {
+    private lateinit var items: List<Person>
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PersonViewHolder {
+        items = personDbHelper.findAll()
         val view = LayoutInflater.from(parent.context)
             .inflate(R.layout.person_list_single_item, parent, false)
 
@@ -24,6 +27,11 @@ class PersonRecyclerAdapter(private var items: List<Person>) :
 
     override fun getItemCount(): Int {
         return items.size
+    }
+
+    fun updateRecyclerView() {
+        items = personDbHelper.findAll()
+        notifyDataSetChanged()
     }
 
     class PersonViewHolder constructor(itemView: View) : RecyclerView.ViewHolder(itemView) {
